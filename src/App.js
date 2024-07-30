5dec5cd8db6765491e3743140f5af2d84c69671f
Dapper724 <tefomaleke97@gmail.com>
2024-07-30 11:02:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import TodoList from './components/TodoList';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import {IoAdd, IoRadioButtononoffSharp} from 'react-icons/io5'
+import {FaStar, FaRegStar, FaEdit, FaSave} from 'react-icons/fa'
+import {AiOutlineDelete} from 'react-icons/ai'
+
+function TodoList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoList
